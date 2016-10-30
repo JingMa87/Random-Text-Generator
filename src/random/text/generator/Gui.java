f7435e6b81dacf595c8392a129a7ea24a7a0767a
Yash Ugrankar <yash.ugrankar@gmail.com>
2016-10-30 20:56:23 +0530

    Add file chooser to GUI
    Issue #17: A button has been added to the GUI, which when clicked creates a JFileChooser object that allows the user to select a text file from which text is fetched. This fetched text is then used to replace the contents of the input text area.

--- src/random/text/generator/Gui.java
+++ src/random/text/generator/Gui.java
@@ -2,10 +2,13 @@ package random.text.generator;
 
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -13,6 +16,9 @@ import javax.swing.JTextArea;
 
 
 public class Gui {
+        private RandomTextGenerator generator;
+        //private static final int DEFAULT_WORD_AMOUNT;
+        
 	public Gui() {
 		init();
 	}
@@ -28,23 +34,57 @@ public class Gui {
 		panel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
 		frame.add(panel);
 
-		JLabel label = new JLabel("Click the button on the bottom!");
-		label.setAlignmentX(Component.CENTER_ALIGNMENT);
-		panel.add(label);
-		
+		JLabel headerLabel = new JLabel("Click the button on the bottom!");
+		headerLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+		panel.add(headerLabel);
+                
+                JComboBox<String> generatorSelectBox = new JComboBox<>();
+                generatorSelectBox.addItem("SpaceEfficientGenerator");
+                generatorSelectBox.addItem("SpeedEfficientGenerator");
+                generatorSelectBox.setAlignmentX(Component.RIGHT_ALIGNMENT);
+                panel.add(generatorSelectBox);
+                
 		JTextArea inputTextArea = new JTextArea(5, 5);
 		inputTextArea.setText(RandomTextGenerator.getDefaultText());
 		inputTextArea.setAlignmentX(Component.CENTER_ALIGNMENT);
+                inputTextArea.setLineWrap(true);
+                inputTextArea.setWrapStyleWord(true);
 		panel.add(inputTextArea);
                 
                 JTextArea outputTextArea = new JTextArea(5, 5);
-		outputTextArea.setText("Not randomly generated text.");
+		outputTextArea.setText("Result");
+                outputTextArea.setEditable(false);
 		outputTextArea.setAlignmentX(Component.CENTER_ALIGNMENT);
+                outputTextArea.setLineWrap(true);
+                outputTextArea.setWrapStyleWord(true);
 		panel.add(outputTextArea);
 		
 		JButton generateTextButton = new JButton("Generate text");
 		generateTextButton.setAlignmentX(Component.CENTER_ALIGNMENT);
 		panel.add(generateTextButton);
+                
+                JLabel executionTimeLabel = new JLabel();
+		panel.add(executionTimeLabel);
+                
+                generateTextButton.addActionListener(new ActionListener(){
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        String generatorType = (String)generatorSelectBox.getSelectedItem();
+                        String inputText = inputTextArea.getText();
+                        switch(generatorType){
+                            case "SpeedEfficientGenerator":
+                                generator = new SpeedEfficientGenerator(inputText);
+                                break;
+                            default:
+                                generator = new SpaceEfficientGenerator(inputText);
+                        }
+                        long startTime = System.currentTimeMillis();
+                        outputTextArea.setText(generator.generateText(200));
+                        long stopTime = System.currentTimeMillis();
+                        long elapsedTime = stopTime - startTime;
+                        executionTimeLabel.setText("Time taken: " + elapsedTime + "ms");
+                    }
+                });
 		
 		frame.pack();
 		frame.setLocationRelativeTo(null);
